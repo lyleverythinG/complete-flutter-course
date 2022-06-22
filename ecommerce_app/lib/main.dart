069fe35745b0b24e26bb2fd063fdf306d7de8f3c
lyleverythinG <lylecarcedo@gmail.com>
2022-06-22 14:18:25 +0800

    added simple go_routing functionality

--- ecommerce_app/lib/main.dart
+++ ecommerce_app/lib/main.dart
@@ -2,11 +2,14 @@ import 'dart:async';
 
 import 'package:ecommerce_app/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // https://docs.flutter.dev/testing/errors
   await runZonedGuarded(() async {
+    //remove hash # in the url
+    GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
     runApp(const MyApp());
 
     FlutterError.onError = (FlutterErrorDetails details) {
---
+++ ecommerce_app/lib/src/routing/app_router.dart
@@ -0,0 +1,57 @@
+import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
+import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_screen.dart';
+import 'package:ecommerce_app/src/features/sign_in/email_password_sign_in_state.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../features/products_list/products_list_screen.dart';
+
+// follow the KISS principle. Keep things Simple.
+final goRouter = GoRouter(
+  // this is set in default but just in case you want to modify this one.
+  initialLocation: '/',
+  debugLogDiagnostics: true,
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const ProductsListScreen(),
+      routes: [
+        //don't use / for subroutes
+        GoRoute(
+          path: 'cart',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const ShoppingCartScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'orders',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const OrdersListScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'account',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const AccountScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'signIn',
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const EmailPasswordSignInScreen(
+                formType: EmailPasswordSignInFormType.signIn),
+          ),
+        ),
+      ],
+    ),
+  ],
+);
